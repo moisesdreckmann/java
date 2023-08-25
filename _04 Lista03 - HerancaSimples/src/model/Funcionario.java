084d75a118471a4ce894c2808c4d8a821d6e30ed
Anark <dreckmannmoises@gmail.com>
2023-08-25 00:32:15 -0300

    iniciei herança

---
+++ _04 Lista03 - HerancaSimples/src/model/Funcionario.java
@@ -0,0 +1,34 @@
+package model;
+
+public abstract class Funcionario {
+    private String nome;
+    private double salario;
+
+    public Funcionario() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "\nFuncionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
